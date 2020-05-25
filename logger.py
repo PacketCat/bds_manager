d457ca5f106dbb13b9da4a8542c03ded0028f554
Cat in Packet <42083774+PacketCat@users.noreply.github.com>
2020-05-25 20:18:03 +0300

    Some changes for logger

--- logger.py
+++ logger.py
@@ -3,10 +3,10 @@ import time, datetime
 class Logging:
 	def __init__(self, filename):
 		dt = datetime.datetime.today()
-		self.fd = open('./logs/{}-{}-{}_{}:{}:{}.log'.format(dt.day, dt.month, dt.year, dt.hours, dt.minutes.seconds), 'a')
+		self.fd = open('./logs/{}-{}-{}_{}:{}:{}.log'.format(dt.day, dt.month, dt.year, dt.hour, dt.minute, dt.second), 'a')
 		self.closed = False
 		self.polllist = []
-	def _write(self, data, state):
+	def _write(self, state, data):
 		prefix = 0
 		if state == 'main':
 			prefix = 0
@@ -14,15 +14,19 @@ class Logging:
 			prefix = 1
 		if state == 'console':
 			prefix = 2
-		self.fd.write(str(prefix) + data)
+		self.fd.write(str(prefix) + data + '\n')
+		self.fd.flush()
 		for i in self.polllist:
 			i.add(str(prefix)+data)
 	def error(self, state, string):
 		dt = datetime.datetime.today()
-		self._write(state, '[{}-{}-{}_{}:{}:{}]:[ERROR] :: {}'.format(dt.day, dt.month, dt.year, dt.hours, dt.minutes.seconds, string))
+		self._write(state, '[{}-{}-{} {}:{}:{} ERROR] :: {}'.format(dt.day, dt.month, dt.year, dt.hour, dt.minute, dt.second, string))
 	def info(self, state, string):
 		dt = datetime.datetime.today()
-		self._write(state, '[{}-{}-{}_{}:{}:{}]:[INFO] :: {}'.format(dt.day, dt.month, dt.year, dt.hours, dt.minutes.seconds, string))
+		self._write(state, '[{}-{}-{} {}:{}:{} INFO] :: {}'.format(dt.day, dt.month, dt.year, dt.hour, dt.minute, dt.second, string))
+	def raw(self, string):
+		self._write('server', string[:-1])
+
 	def close(self):
 		self.fd.close()
 		self.closed = True
